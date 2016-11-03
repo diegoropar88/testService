7612edced0728cd6d3bc36c52f3402245dec89a9
sala-a <sala-a@11C01E14115955>
2016-11-02 19:21:13 -0500

    first Version

---
+++ testservice/src/app/webservices/micro.java
@@ -0,0 +1,14 @@
+package app.webservices;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService()
+public class micro {
+
+	@WebMethod()
+	public String sayHello(String name) {
+	    System.out.println("Hello: " + name);
+	    return "Hello " + name + "!";
+	}
+}
